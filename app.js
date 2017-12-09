11d12bec24b585cc159b624871790e7cfa77c45a
Michael Matthews <michaelmatthews@Michaels-MacBook-Pro.local>
2017-12-09 14:10:59 -0700

    updated output

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const port = process.env.PORT;
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
   res.sendfile('index.html', { root: __dirname + "/public" } );
 });
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
